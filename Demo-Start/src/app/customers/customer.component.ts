da475d40344a9a0755c8d1bce64e056ce0fcaffa
Manishkr117 <manishkr117@gmail.com>
2019-09-27 23:28:24 +0530

    Before using Form Builder

--- Demo-Start/src/app/customers/customer.component.ts
+++ Demo-Start/src/app/customers/customer.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
-import { NgForm } from '@angular/forms';
+import {FormGroup, FormControl} from '@angular/forms';
+
 
 import { Customer } from './customer';
 
@@ -9,15 +10,45 @@ import { Customer } from './customer';
   styleUrls: ['./customer.component.css']
 })
 export class CustomerComponent implements OnInit {
+  customerForm: FormGroup;
   customer = new Customer();
 
   constructor() { }
 
   ngOnInit() {
+    this.customerForm = new FormGroup({
+      firstName:new FormControl(),
+      lastName:new FormControl(),
+      email: new FormControl(),
+      sendCatalog:new FormControl(true)
+      
+    });
   }
 
-  save(customerForm: NgForm) {
-    console.log(customerForm.form);
-    console.log('Saved: ' + JSON.stringify(customerForm.value));
+  save() {
+    console.log(this.customerForm);
+    console.log('Saved: ' + JSON.stringify(this.customerForm.value));
+  }
+
+
+
+  // PopulateTestData(){
+  //   console.log("Populate test data was called");
+  //   this.customerForm.setValue({
+  //     firstName:"MK",
+  //     lastName: "K",
+  //     email:"manishkr117@gmail.com",
+  //     sendCatalog: false
+  //   })
+  // }
+
+  PopulateTestData(){
+    console.log("Populate test data was called");
+    this.customerForm.patchValue({
+      firstName:"MK",
+      lastName: "K",
+      // email:"manishkr117@gmail.com",
+      sendCatalog: false
+    })
   }
 }
